5c2d3e31a62586b2d5bc020d90627b78363b433c
Jakub146480 <128227074+Jakub146480@users.noreply.github.com>
2023-06-25 08:20:53 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1, y1, x2, y2;
+
+        Console.WriteLine("Podaj współrzędne punktu początkowego (x1, y1):");
+        x1 = Convert.ToDouble(Console.ReadLine());
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj współrzędne punktu końcowego (x2, y2):");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        double distance = CalculateDistance(x1, y1, x2, y2);
+        Console.WriteLine("Długość odcinka: " + distance);
+    }
+
+    static double CalculateDistance(double x1, double y1, double x2, double y2)
+    {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        double distance = Math.Sqrt(dx * dx + dy * dy);
+        return distance;
+    }
+}
